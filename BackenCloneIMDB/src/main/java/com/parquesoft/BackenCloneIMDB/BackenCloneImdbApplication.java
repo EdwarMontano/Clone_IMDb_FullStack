51661a67b488ce4937e36d13e44389e8122f93d0
Edwar Montano <edwar.montano@correounivalle.edu.co>
2023-10-12 20:35:24 -0500

    [create]

---
+++ BackenCloneIMDB/src/main/java/com/parquesoft/BackenCloneIMDB/BackenCloneImdbApplication.java
@@ -0,0 +1,13 @@
+package com.parquesoft.BackenCloneIMDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenCloneImdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenCloneImdbApplication.class, args);
+	}
+
+}
